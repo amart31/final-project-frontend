dc7bf5eb90de9d2f5ecf1c3da45d34e7cdb37e13
Angelo Martinez <amart670@fiu.edu>
2019-04-16 00:04:09 +0000

    fixed front page jumbotron

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 let imgUrl =
 	"https://images.pexels.com/photos/1345082/pexels-photo-1345082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
@@ -7,20 +11,25 @@ const jumbostyle = {
 	backgroundSize: "cover",
 	color: "#4a82db",
 	height: "500px",
-	textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
+	position: "relative"
+};
+const h2Style = {
+	position: "absolute",
+	bottom: "60px",
+	left: "50px"
 };
 
 const Jumbotron = () => {
 	return (
 		<div className="jumbotron jumbotron-fluid" style={jumbostyle}>
 			<div className="container">
-				<h1 className="display-4">Welcome to ReactCloset</h1>
-				<p className="lead">
-					This is a modified jumbotron designed to inspire
-				</p>
-				<button type="button" className="btn btn-success">
-					Shop
-				</button>
+				<h2 className="text-uppercase align-middle" style={h2Style}>
+					Shop latest drop
+					{"  "}
+					<Link to="/rent">
+						<FontAwesomeIcon icon={faArrowCircleRight} />
+					</Link>
+				</h2>
 			</div>
 		</div>
 	);
