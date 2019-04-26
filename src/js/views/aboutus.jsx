67bb00bd3d8d682dd55c52fa25cf29afa3cc7ee7
Angelo Martinez <amart670@fiu.edu>
2019-04-26 23:30:06 +0000

    MVP about us page

--- src/js/views/aboutus.jsx
+++ src/js/views/aboutus.jsx
@@ -16,13 +16,14 @@ const titleStyle = {
 	letterSpacing: "4px",
 	left: "0",
 	right: "0",
-	top: "75%"
+	top: "80%"
 };
 
 const headingStyle = {
 	fontWeight: "300",
 	fontSize: "32px",
-	color: "#4a4a4a"
+	color: "#4a82db",
+	letterSpacing: "2px"
 };
 
 const pStyle = {
@@ -36,23 +37,40 @@ const IMGStyle = {
 	borderRadius: "50%"
 };
 
+const techLogo = {
+	height: "100px"
+};
+
 export class About extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-3">
 				<Jumbotron style={style} fluid={true}>
-					<h1 style={titleStyle}>About React Closet</h1>
-					<hr className="my-2" />
+					<h1 className="display-3" style={titleStyle}>
+						About React Closet
+					</h1>
 				</Jumbotron>
 				<h2 style={headingStyle}>The luxury closet reimagined</h2>
 				<p className="pt-3" style={pStyle}>
 					{" "}
-					React closet aims to bring about luxury clothing to the 21st
-					century. Created with the sole purpose of making looking
-					good not a full time hustle
+					We built this website with a whole lot of love. It all
+					started as a passion for creating things. We were chilling
+					one day and decided to take a plunge at coding our own
+					business ideas because why not it couldnt be that hard.
+					Thanks to{" "}
+					<a href="https://www.4geeksacademy.co/">@4geekAcademy</a> me
+					and my brother have been able to finally crush it and
+					finally we built, from the ground up our very first business
+					idea. React closet aims to bring about luxury clothing to
+					the 21st century. Created with the sole purpose of making
+					looking good not a full time hustle
 				</p>
-				<h2 style={headingStyle}> Creators</h2>
-				<Row>
+				<h2 style={headingStyle} className="mb-4">
+					{" "}
+					Creators
+				</h2>
+				<Row className="mb-5">
+					<Col />
 					<Col>
 						<img
 							src="https://avatars3.githubusercontent.com/u/46979797?s=460&v=4"
@@ -71,25 +89,79 @@ export class About extends React.Component {
 							Angelo
 						</div>
 					</Col>
+					<Col />
 				</Row>
-				<Jumbotron fluid={true}>
-					<h1 className="display-3">Technologies Used</h1>
+				<Jumbotron fluid={true} style={{ height: "575px" }}>
+					<h1 className="display-3" style={{ color: "#4a82db" }}>
+						Technologies Used
+					</h1>
 					<p className="lead">
-						This is a simple hero unit, a simple Jumbotron-style
-						component for calling extra attention to featured
-						content or information.
+						The technologies that we have learned have enabled us to
+						bring about our dreams. The following is a list of the
+						technologies and libraries that we mostly used for this
+						project.
 					</p>
 					<hr className="my-2" />
-					<ul>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>Bootstrap</li>
-						<li>React.JS</li>
-						<li>PHP</li>
-						<li>Wordpress</li>
-						<li>Webpack</li>
-						<li>Git</li>
-					</ul>
+					<Row>
+						<Col>
+							<img
+								src="http://downloadicons.net/sites/default/files/html5-logo-icon-47404.png"
+								style={techLogo}
+							/>
+							<div>HTML</div>
+						</Col>
+						<Col>
+							<img
+								src="https://cdn.freebiesupply.com/logos/large/2x/css-3-logo-png-transparent.png"
+								style={techLogo}
+							/>
+							<div>CSS</div>
+						</Col>
+						<Col>
+							<img
+								src="http://www.logospng.com/images/68/bootstrap-logo-system-4-68467.png"
+								style={techLogo}
+							/>
+							<div>Bootstrap</div>
+						</Col>
+						<Col>
+							<img
+								src="https://arcweb.co/wp-content/uploads/2016/10/react-logo-1000-transparent.png"
+								style={techLogo}
+							/>
+							<div>ReactJS</div>
+						</Col>
+					</Row>
+					<Row className="mt-5">
+						<Col>
+							<img
+								src="https://cdn.freebiesupply.com/logos/large/2x/php-logo-png-transparent.png"
+								style={techLogo}
+							/>
+							<div>PHP</div>
+						</Col>
+						<Col>
+							<img
+								src="https://www.momento360.com/wp-content/uploads/2018/04/wordpress-logo-transparent-background-10.png"
+								style={techLogo}
+							/>
+							<div>Wordpress</div>
+						</Col>
+						<Col>
+							<img
+								src="https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png"
+								style={techLogo}
+							/>
+							<div>Webpack</div>
+						</Col>
+						<Col>
+							<img
+								src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+								style={techLogo}
+							/>
+							<div>Git</div>
+						</Col>
+					</Row>
 				</Jumbotron>
 			</div>
 		);
