c20e7210e394278f95fccf5604c6477b9f25ae31
Andy Martinez <martinez1751@gmail.com>
2019-04-27 16:03:04 +0000

    confirm pw validation

--- src/js/component/shortSignUp.jsx
+++ src/js/component/shortSignUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Form, FormGroup, Label, Input, Button, Row } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button, NavLink } from "reactstrap";
 import { Context } from "../store/appContext.jsx";
 
 export default class ShortSignupForm extends React.Component {
@@ -7,14 +7,33 @@ export default class ShortSignupForm extends React.Component {
 		super(props);
 
 		this.state = {
-			user: "",
+			username: "",
 			email: "",
-			pass: ""
+			pass: "",
+			confirmPass: ""
 		};
-		this.user = React.createRef();
+		this.username = React.createRef();
 		this.email = React.createRef();
 		this.pass = React.createRef();
+		this.confirmPass = React.createRef();
 	}
+	validateForm() {
+		return (
+			this.state.username.length > 0 &&
+			this.state.pass.length > 0 &&
+			this.state.pass == this.state.confirmPass
+		);
+	}
+
+	// check() {
+	// 	if (this.pass == this.confirmPass) {
+	// 		document.getElementById("message").innerHTML = "matching";
+	// 		document.getElementById("message").style.color = "green";
+	// 	} else if (this.pass !== this.confirmPass) {
+	// 		document.getElementById("message").innerHTML = "not matching";
+	// 		document.getElementById("message").style.color = "red";
+	// 	}
+	// }
 	handleChange(event) {
 		this.setState({ value: event.target.value });
 	}
@@ -23,7 +42,6 @@ export default class ShortSignupForm extends React.Component {
 			<div
 				style={{
 					background: "#a9b6cc",
-					color: "#4a82db",
 					height: "-webkit-fill-available"
 				}}>
 				<Context.Consumer>
@@ -32,86 +50,119 @@ export default class ShortSignupForm extends React.Component {
 							return alert("Welcome " + store.session.user);
 						} else {
 							return (
-								<div className="container">
+								<div
+									className="container text-center border"
+									style={{
+										width: "400px",
+										color: "#4a82db",
+										background: "white"
+									}}>
+									<h2 className="mt-5 pt-3">Sign Up</h2>
 									<Form>
-										<Row form>
-											<Col xs={12} md={4}>
-												<FormGroup>
-													<Label htmlFor="user">
-														Username
-													</Label>
-													<Input
-														type="text"
-														name="user"
-														id="user"
-														value={this.state.user}
-														ref={this.user}
-														placeholder=""
-														onChange={e =>
-															this.setState({
-																user:
-																	e.target
-																		.value
-															})
-														}
-													/>
-												</FormGroup>
-											</Col>
-											<Col xs={12} md={4}>
-												<FormGroup>
-													<Label htmlFor="email">
-														Email
-													</Label>
-													<Input
-														type="email"
-														name="email"
-														id="email"
-														value={this.state.email}
-														ref={this.email}
-														placeholder="example@mail.com"
-														onChange={e =>
-															this.setState({
-																email:
-																	e.target
-																		.value
-															})
-														}
-													/>
-												</FormGroup>
-											</Col>
-											<Col xs={12} md={4}>
-												<FormGroup>
-													<Label htmlFor="userPass">
-														Password
-													</Label>
-													<Input
-														type="password"
-														name="userPass"
-														id="userPass"
-														ref={this.pass}
-														value={this.state.pass}
-														placeholder="password placeholder"
-														onChange={e =>
-															this.setState({
-																pass:
-																	e.target
-																		.value
-															})
-														}
-													/>
-												</FormGroup>
-											</Col>
-										</Row>
+										<FormGroup>
+											<Label htmlFor="username">
+												Username:
+											</Label>
+											<Input
+												style={{
+													width: "300px",
+													margin: "0 auto"
+												}}
+												type="text"
+												name="username"
+												id="username"
+												ref={this.username}
+												placeholder=""
+												onChange={e =>
+													this.setState({
+														username: e.target.value
+													})
+												}
+											/>
+										</FormGroup>
 
-										<div className="text-center pb-5 mb-5">
+										<FormGroup>
+											<Label htmlFor="email">
+												Email:
+											</Label>
+											<Input
+												style={{
+													width: "300px",
+													margin: "0 auto"
+												}}
+												type="email"
+												name="email"
+												id="email"
+												ref={this.email}
+												placeholder="example@mail.com"
+												onChange={e =>
+													this.setState({
+														email: e.target.value
+													})
+												}
+											/>
+										</FormGroup>
+
+										<FormGroup>
+											<Label htmlFor="pass">
+												Password:
+											</Label>
+											<Input
+												style={{
+													width: "300px",
+													margin: "0 auto"
+												}}
+												type="password"
+												name="pass"
+												id="pass"
+												ref={this.pass}
+												// onkeyup={this.check()}
+												placeholder="password"
+												onChange={e =>
+													this.setState({
+														pass: e.target.value
+													})
+												}
+											/>
+										</FormGroup>
+
+										<FormGroup>
+											<Label htmlFor="confirmPass">
+												Confirm Password:
+											</Label>
+											<Input
+												style={{
+													width: "300px",
+													margin: "0 auto"
+												}}
+												type="password"
+												name="confirmPass"
+												id="confirmPass"
+												ref={this.confirmPass}
+												// onkeyup={this.check()}
+												placeholder="password placeholder"
+												onChange={e =>
+													this.setState({
+														confirmPass:
+															e.target.value
+													})
+												}
+											/>
+											<span id="message" />
+										</FormGroup>
+
+										<div className="text-center pb-2">
 											<Button
 												color="primary"
+												disabled={!this.validateForm()}
 												onClick={() => {
 													this.setState({
 														users: actions.register(
 															this.state.user,
 															this.state.pass,
-															this.state.email
+															this.state.email,
+															this.state
+																.confirmPass
 														)
 													});
 												}}>
@@ -119,6 +170,11 @@ export default class ShortSignupForm extends React.Component {
 											</Button>
 										</div>
 									</Form>
+									<div>
+										<NavLink href="/login">
+											Already a Member? Log in
+										</NavLink>
+									</div>
 								</div>
 							);
 						}
