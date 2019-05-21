f8140b7e6b20301c34ae3c6c20bd189bfa870334
Angelo Martinez <amart670@fiu.edu>
2019-05-21 17:57:08 +0000

    finished scss transfer of subhead section

--- src/js/component/SubHead.js
+++ src/js/component/SubHead.js
@@ -1,63 +1,32 @@
 import React from "react";
-import SubHeadCard from "./functional/SubHeadCard";
 import { Link } from "react-router-dom";
 
 const SubHead = props => {
 	return (
 		<div className="subhead-container mb-3 mt-3">
 			<Link to="/contact">
-				<div className="d-inline-block m-3">
-					<SubHeadCard
-						style={{
-							border: "1px solid #4a82db",
-							color: "#4a82db"
-						}}
-						text="Contact Us"
-					/>
+				<div className="d-inline-block subheadbutton mr-4">
+					<div className="subhead-text">Contact Us</div>
 				</div>
 			</Link>
 			<Link to="/rent">
-				<div className="d-inline-block m-3">
-					<SubHeadCard
-						style={{
-							border: "1px solid #4a82db",
-							color: "#4a82db"
-						}}
-						text="Shop the feed"
-					/>
+				<div className="d-inline-block subheadbutton mr-4">
+					<div className="subhead-text">Shop the feed</div>
 				</div>
 			</Link>
 			<Link to="/login">
-				<div className="d-inline-block m-3">
-					<SubHeadCard
-						text="Start Selling"
-						style={{
-							border: "1px solid #4a82db",
-							color: "#4a82db"
-						}}
-					/>
+				<div className="d-inline-block subheadbutton mr-4">
+					<div className="subhead-text">Start Selling</div>
 				</div>
 			</Link>
 			<Link to="/insurance">
-				<div className="d-inline-block m-3">
-					<SubHeadCard
-						text="Insurance Policy"
-						style={{
-							border: "1px solid #4a82db",
-							color: "#4a82db"
-						}}
-					/>
+				<div className="d-inline-block subheadbutton mr-4">
+					<div className="subhead-text">Insurance Policy</div>
 				</div>
 			</Link>
 			<Link to="/aboutus">
 				<div className="d-inline-block m-3">
-					<SubHeadCard
-						text="About Us"
-						style={{
-							border: "1px solid #4a82db",
-							color: "#4a82db"
-						}}
-					/>
+					<div className="subhead-text">About Us</div>
 				</div>
 			</Link>
 		</div>
